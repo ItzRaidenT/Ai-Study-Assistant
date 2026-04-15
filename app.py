bd473d8b136b3fa6c1f45eee9c106101540682ac
RionaLoo <looriona@gmail.com>
2026-04-15 15:07:42 +0800

    riona1

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class filedatabase(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(100), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def _repr_(self) -> str:
+        return f" File {self.id} "
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    hxjewhkcxhe
+    
